f5348cd94dfd10e7fcd0551a9af61b67ecac2dad
Colm McBarron <colm.mcbarron@iqcontent.com>
2013-11-22 17:38:10 +0000

    Add callback for instagram api

--- web.rb
+++ web.rb
@@ -5,3 +5,18 @@ require 'sinatra/json'
 get '/' do
   "Hello clock!"
 end
+
+get '/clock_callback/:clock_id' do 
+	params["hub.challenge"]
+end
+
+post '/clock_callback/:clock_id' do 
+	
+end
+
+
+
+
+
+# c8ef24044b394ea0b11dbfd34dbffa45
+# 0b321ddbe9864840a0472b4a8a15e380
