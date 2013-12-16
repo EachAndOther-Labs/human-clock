460594cdf1d74ef2982331ea0539792cc54f88e0
Richey Ryan <richey.ryan@iqcontent.com>
2013-12-16 20:14:09 +0000

    port change

--- app.js
+++ app.js
@@ -1,142 +1,86 @@
-/*var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-
-app.listen(8111);
-
-function handler (req, res) {
-
-  // if (typeof res.query != "undefined") {
-      console.log(req);
-  //}
-
-/*  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-
-}
-*/
-/*
-
-var express = require("express");
-var app = require('express')()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
-
-app.use(express.logger());
-// app.use(express.bodyParser());
-*/
-/*
-*/
-
-/*
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-
-
-*/
-
-/*
-
-
-io.sockets.on('connection', function (socket) {
-  socket.on('send', function (data) {
-    io.sockets.emit('message', data);
-  });
-});
-*/
-
-
-
-var express = require('express')
-  , app = express()
-  , http = require('http')
-  , https = require('https')
-  , server = http.createServer(app)
-  , io = require('socket.io').listen(server);
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    https = require('https'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
 var url = require('url');
 
 var port = process.env.PORT || 5000;
 server.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
 
-app.use(function(req, res, next){
-   var data = "";
-   req.on('data', function(chunk){ data += chunk});
-   req.on('end', function(){
-      req.rawBody = data;
-      next();
-   });
+app.use(function(req, res, next) {
+    var data = "";
+    req.on('data', function(chunk) {
+        data += chunk
+    });
+    req.on('end', function() {
+        req.rawBody = data;
+        next();
+    });
 });
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
 });
 
 app.get('/clock_callback/1', function(request, response) {
-  var url_parts = url.parse(request.url, true);
-  var query = url_parts.query;
-  if (query['hub.verify_token'] == 'update_iq_human_clock') {
-    response.send(query['hub.challenge']);
-  }
+    var url_parts = url.parse(request.url, true);
+    var query = url_parts.query;
+    if (query['hub.verify_token'] == 'update_iq_human_clock') {
+        response.send(query['hub.challenge']);
+    }
 });
 
 app.post('/clock_callback/1', function(request, response) {
     var instagram_updates = JSON.parse(request.rawBody);
     for (var i in instagram_updates) {
-      console.log(instagram_updates[i]);
+        console.log(instagram_updates[i]);
     }
 
     var numDataUpdates = instagram_updates.length;
 
     var options = {
-      host: 'api.instagram.com',
-      port: 443,
-      path: '/v1/tags/London/media/recent?client_id=c8ef24044b394ea0b11dbfd34dbffa45&client_secret=0b321ddbe9864840a0472b4a8a15e380'
+        host: 'api.instagram.com',
+        port: 80,
+        path: '/v1/tags/London/media/recent?client_id=c8ef24044b394ea0b11dbfd34dbffa45&client_secret=0b321ddbe9864840a0472b4a8a15e380'
     };
 
     var req = https.request(options, function(res) {
-      console.log('STATUS: ' + res.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      var fullData = "";
-      res.on('data', function (chunk) {
-        fullData += chunk;
-      });
-      res.on('end', function(){
-        try {
-          var fullJSONData = JSON.parse(fullData);
-          if (fullJSONData.data.length < numDataUpdates) {
-            numDataUpdates = fullJSONData.data.length;
-          }
-          var updatesArray = [];
-          for (var i=0; i<numDataUpdates;i++) {
-            var update = {
-              image: fullJSONData.data[i].images.low_resolution.url,
-              id: fullJSONData.data[i].id
-            };
-            io.sockets.emit('instagram', update);
-          }
+        console.log('STATUS: ' + res.statusCode);
+        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        res.setEncoding('utf8');
+        var fullData = "";
+        res.on('data', function(chunk) {
+            fullData += chunk;
+        });
+        res.on('end', function() {
+            try {
+                var fullJSONData = JSON.parse(fullData);
+                if (fullJSONData.data.length < numDataUpdates) {
+                    numDataUpdates = fullJSONData.data.length;
+                }
+                var updatesArray = [];
+                for (var i = 0; i < numDataUpdates; i++) {
+                    var update = {
+                        image: fullJSONData.data[i].images.low_resolution.url,
+                        id: fullJSONData.data[i].id
+                    };
+                    io.sockets.emit('instagram', update);
+                }
 
-        } catch(e) {
-            console.log("Error parsing JSON data");
-        }
+            } catch (e) {
+                console.log("Error parsing JSON data");
+            }
 
-      });
+        });
     });
 
     req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
+        console.log('problem with request: ' + e.message);
     });
 
     // write data to request body
@@ -149,9 +93,9 @@ app.post('/clock_callback/1', function(request, response) {
 
 app.use(express.static(__dirname + '/static'));
 
-io.sockets.on('connection', function (socket) {
-  io.sockets.emit("init", "New York");
-    socket.on('instagram', function (data) {
+io.sockets.on('connection', function(socket) {
+    io.sockets.emit("init", "New York");
+    socket.on('instagram', function(data) {
         io.sockets.emit('instagram', data);
     });
 });
