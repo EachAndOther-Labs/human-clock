3b6d914908989a9e7739ce3a24c2de601b2de0a0
Richey Ryan <richey.ryan@iqcontent.com>
2014-01-07 00:50:53 +0000

    fix dupes

--- static/humanclock.js
+++ static/humanclock.js
@@ -37,7 +37,7 @@ HumanClock = {
             //console.log("receiving socket data");
             var stringObj = JSON.stringify(data);
             s.images.push(stringObj);
-            _.uniq(s.images);
+            s.images = .uniq(s.images);
         });
         setInterval(HumanClock.render, 1500);
     },
